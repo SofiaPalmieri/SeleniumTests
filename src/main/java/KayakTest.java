7c262986805386b82a890dfcc9a5f7258cd5cec2
SofiaPalmieri <spalmieri@frba.utn.edu.ar>
2022-10-28 17:40:47 -0300

    Refactoring

--- src/main/java/KayakTest.java
+++ src/main/java/KayakTest.java
@@ -9,10 +9,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class KayakTest {
         WebDriver driver;
+
+        DateTimeFormatter monthFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyy");
+
+        DateTimeFormatter shortDateFormatter = DateTimeFormatter.ofPattern("E. d/M");
         static WebDriverManager wdm;
 
         @BeforeAll
@@ -35,49 +46,46 @@ public class KayakTest {
         @Test
         void test() throws InterruptedException {
             // Exercise
+            LocalDate startCalendar = LocalDate.now().plusMonths(2);
+            LocalDate endCalendar = LocalDate.from(startCalendar).plusDays(5);
             driver.get("https://www.kayak.com.ar/");
-            WebElement origenInput = driver.findElement(By.className("k_my-input"));
-            origenInput.click();
-            origenInput.sendKeys(Keys.BACK_SPACE);
-            origenInput.sendKeys(Keys.BACK_SPACE);
-            origenInput.sendKeys("Mar del Plata");
-            origenInput.sendKeys(Keys.ARROW_DOWN);
-            origenInput.sendKeys(Keys.ENTER);
-            origenInput.sendKeys(Keys.TAB);
-            origenInput.sendKeys(Keys.ENTER);
-            origenInput.click();
-            origenInput.sendKeys("Buenos Aires");
-            origenInput.sendKeys(Keys.ARROW_DOWN);
-            origenInput.sendKeys(Keys.ENTER);
+            WebElement originInput = driver.findElement(By.cssSelector(".zEiP-origin input"));
+            originInput.click();
+            originInput.sendKeys(Keys.BACK_SPACE);
+            originInput.sendKeys(Keys.BACK_SPACE);
+            originInput.sendKeys("Buenos Aires");
+            originInput.sendKeys(Keys.ARROW_DOWN);
+            originInput.sendKeys(Keys.ENTER);
+            WebElement destinationInput = driver.findElement(By.cssSelector(".zEiP-destination input"));
+            destinationInput.sendKeys("Mar del Plata");
+            destinationInput.sendKeys(Keys.ARROW_DOWN);
+            destinationInput.sendKeys(Keys.ENTER);
             WebElement startDateField = driver.findElement(By.className("sR_k-value"));
             startDateField.click();
-            WebElement month = driver.findElement(By.className("wHSr-monthName"));
-            boolean containsMonth = month.getText().contains("diciembre 2022");
-            while (!containsMonth){
-                    WebElement nextMonth = driver.findElement(By.cssSelector("[aria-label='Mes siguiente']"));
-                    nextMonth.click();
-                WebElement month2 = driver.findElement(By.className("wHSr-monthName"));
-                containsMonth = month2.getText().contains("diciembre 2022");
-            }
-            WebElement startDate = driver.findElement(By.cssSelector("[aria-label='9 de diciembre de 2022']"));
-            startDate.click();
-            WebElement endDate = driver.findElement(By.cssSelector("[aria-label='14 de diciembre de 2022']"));
-            endDate.click();
-
+            this.selectOnCalendar(startCalendar);
+            this.selectOnCalendar(endCalendar);
             WebElement searchButton = driver.findElement(By.className("c8LPF-icon"));
+            WebDriverWait waitUntilSearchButtonIsClickeable = new WebDriverWait(driver, Duration.ofSeconds(2));
+            waitUntilSearchButtonIsClickeable.until(ExpectedConditions.elementToBeClickable(searchButton));
             searchButton.click();
-
-            WebElement results = driver.findElement(By.className("sR_k-date"));
-            System.out.println(results.getText());
-            if(results.getText().contains("vie. 9/12")){
-                System.out.println("Pass");
-            }else{
-                System.out.println("Fail");
-            }
-
+            WebElement resultStartDate = driver.findElement(By.className("sR_k-date"));
+            Assertions.assertEquals(shortDateFormatter.format(startCalendar), resultStartDate.getText());
 
         }
+        void selectOnCalendar(LocalDate date){
+            String monthStr = monthFormatter.format(date);
+            String dayStr = dayFormatter.format(date);
+            List<WebElement> months = driver.findElements(By.cssSelector("[data-month]"));
+            Optional<WebElement> monthElement = months.stream().filter(m -> monthStr.equals(m.getAttribute("data-month"))).findFirst();
+            while (monthElement.isEmpty()){
+                WebElement nextMonth = driver.findElement(By.cssSelector(".ATGJ-next-month button"));
+                nextMonth.click();
+                months = driver.findElements(By.cssSelector("[data-month]"));
+                monthElement = months.stream().filter(m -> monthStr.equals(m.getAttribute("data-month"))).findFirst();
+            }
 
+            monthElement.get().findElement(By.cssSelector("[aria-label='"+dayStr+"']")).click();
+        }
 
     }
 
